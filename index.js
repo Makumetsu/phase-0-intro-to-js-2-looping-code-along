de8dc6690f15b95d9636b28aac16bb5dc13d489d
Mohamed Osoble <ossoble360@gmail.com>
2023-09-12 21:08:33 -0400

    countdown and thank you notes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+  function writeCards(names) {
+    const thankyou = []
+   for (let i = 0; i < names.length;i++){
+        thankyou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)   
+        
+    }
+    return(thankyou)
+}
+    const qz = 10
+   function countDown(qz) {
+    while (qz >= 0){
+            console.log(qz)
+            qz-- 
+            
+        }
+
+    }
